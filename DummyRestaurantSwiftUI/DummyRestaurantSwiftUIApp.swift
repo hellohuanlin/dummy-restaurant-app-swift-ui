213e773b575937c343e1818bc0c6ab2d559abdaa
Huan Lin <hellohuanlin@gmail.com>
2021-12-20 10:16:02 -0800

    Initial Commit

---
+++ DummyRestaurantSwiftUI/DummyRestaurantSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  DummyRestaurantSwiftUIApp.swift
+//  DummyRestaurantSwiftUI
+//
+//  Created by Huan Lin on 12/20/21.
+//
+
+import SwiftUI
+
+@main
+struct DummyRestaurantSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ DummyRestaurantSwiftUITests/DummyRestaurantSwiftUITests.swift
@@ -0,0 +1,33 @@
+//
+//  DummyRestaurantSwiftUITests.swift
+//  DummyRestaurantSwiftUITests
+//
+//  Created by Huan Lin on 12/20/21.
+//
+
+import XCTest
+@testable import DummyRestaurantSwiftUI
+
+class DummyRestaurantSwiftUITests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
